a2acc4c1ca88e34ee4e6ab906dab93287e6f3414
Astralstr1ke <99681023+Astralstr1ke@users.noreply.github.com>
2024-04-23 18:30:33 +0200

    Delete dist directory

--- dist/launch.js
+++
@@ -1,7 +0,0 @@
-import { routes } from './0_routes/routes.js';
-//netstat -ano | find "LISTENING" | find "3000" find process on port
-const port = 3000;
-const server = routes.listen(port, () => {
-    console.log('This server is listening at port:' + port);
-});
-//# sourceMappingURL=launch.js.map
