507ee74f8695d6b931b27413d27b50ada9150815
Jakob Persson <jakper01@easv365.dk>
2024-04-24 11:13:39 +0200

    accepted change from other branch

--- src/launch.ts
+++ src/launch.ts
@@ -1,11 +1,7 @@
 import {routes} from './0_routes/routes.js';
 //netstat -ano | find "LISTENING" | find "3000" find process on port
 
-
-
-//const port = process.env.PORT;
 const port = 3000;
-console.log(port);
 
 const server = routes.listen(port, () =>{
     console.log('This server is listening at port:' + port);
